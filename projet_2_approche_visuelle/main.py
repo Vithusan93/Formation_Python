bd20935fd84f145e465f04b3855bed97cfc8d01b
Vithusan93 <gvithusan@gmail.com>
2024-04-18 13:42:57 +0200

    projet tortue escalier

---
+++ projet_2_approche_visuelle/main.py
@@ -0,0 +1,24 @@
+import turtle
+
+def escalier(taille,nb):
+    
+    t = turtle.Turtle()
+
+    for i in range (0, nb):
+        t.forward(taille)
+        t.left(90)
+        t.forward(taille)
+        t.right(90)
+        #taille = taille - 10
+    t.forward(taille)
+
+    turtle.done()
+
+escalier(60,5)
+
+#t.forward(100)
+#t.left(90)
+# t.forward(50)
+#t.backward(100)
+#t.right(45)
+#t.forward(200)
