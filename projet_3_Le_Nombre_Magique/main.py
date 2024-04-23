2e3c0bb982fcbde61ce5a83100499b5e7cd5c5ae
Vithusan93 <gvithusan@gmail.com>
2024-04-23 18:42:38 +0200

    NMagique demander nombre

--- projet_3_Le_Nombre_Magique/main.py
+++ projet_3_Le_Nombre_Magique/main.py
@@ -1,1 +1,12 @@
-print("hello word")
+
+def demander_nombre(nb_min, nb_max):
+
+    nombre_str = input(f"Quel est le nombre magique entre (entre {nb_min} et {nb_max}) ? " )
+    nombre_int = int(nombre_str)
+    return nombre_int
+    
+
+NOMBRE_MIN=1
+NOMBRE_MAX = 10
+NOMBRE_MAGIQUE = 5
+nombre = demander_nombre(NOMBRE_MIN,NOMBRE_MAX)
