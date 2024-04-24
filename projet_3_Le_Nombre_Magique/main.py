084912fc2962ccb8ba789920b787a4e2db1f9159
Vithusan93 <gvithusan@gmail.com>
2024-04-24 13:24:10 +0200

    NMagique condition

--- projet_3_Le_Nombre_Magique/main.py
+++ projet_3_Le_Nombre_Magique/main.py
@@ -4,9 +4,19 @@ def demander_nombre(nb_min, nb_max):
     nombre_str = input(f"Quel est le nombre magique entre (entre {nb_min} et {nb_max}) ? " )
     nombre_int = int(nombre_str)
     return nombre_int
+
     
 
 NOMBRE_MIN=1
 NOMBRE_MAX = 10
 NOMBRE_MAGIQUE = 5
 nombre = demander_nombre(NOMBRE_MIN,NOMBRE_MAX)
+
+if nombre == NOMBRE_MAGIQUE :
+    print("Bravo vous avez gagné")
+    
+elif nombre > NOMBRE_MAGIQUE:
+    print("Le nombre magique est le plus petit")
+    
+else :
+    print("Le nombre magique est plus grand")
