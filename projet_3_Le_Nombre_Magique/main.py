fbf247977349245dfacbd2f7aa153f8b20826909
Vithusan93 <gvithusan@gmail.com>
2024-04-24 13:32:05 +0200

    NMagique boucle

--- projet_3_Le_Nombre_Magique/main.py
+++ projet_3_Le_Nombre_Magique/main.py
@@ -5,18 +5,19 @@ def demander_nombre(nb_min, nb_max):
     nombre_int = int(nombre_str)
     return nombre_int
 
-    
-
 NOMBRE_MIN=1
 NOMBRE_MAX = 10
 NOMBRE_MAGIQUE = 5
-nombre = demander_nombre(NOMBRE_MIN,NOMBRE_MAX)
 
-if nombre == NOMBRE_MAGIQUE :
-    print("Bravo vous avez gagné")
+nombre = 0
+while not nombre == NOMBRE_MAGIQUE:
+    nombre = demander_nombre(NOMBRE_MIN,NOMBRE_MAX)
+    if nombre == NOMBRE_MAGIQUE :
+        print("Bravo vous avez gagné")
     
-elif nombre > NOMBRE_MAGIQUE:
-    print("Le nombre magique est le plus petit")
+    elif nombre > NOMBRE_MAGIQUE:
+        print("Le nombre magique est le plus petit")
     
-else :
-    print("Le nombre magique est plus grand")
+    else :
+        print("Le nombre magique est plus grand")
+    
